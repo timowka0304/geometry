d26ecf7e37eadc90785e610f4e0105a66c8d1939
timowka0304 <timoti20@mail.ru>
2019-03-18 20:47:30 +0700

    Change file-stream

--- main.cpp
+++ main.cpp
@@ -1,37 +1,67 @@
+#include <cmath>
 #include <cstdio>
 #include <cstring>
+#include <fstream>
 #include <iostream>
 
 using namespace std;
+
+ifstream F("input.txt");
+ofstream file("output.txt");
 
 struct Circule {
     float x;
     float y;
     float radius;
-} curcule;
+    float perimeter;
+    float area;
+};
 
-string s;
+Circule circule;
 
-void InputList()
+void print(int i)
 {
-    string t;
-    freopen("input.txt", "r", stdin);
-    while (getline(cin, t)) {
-        s += t;
-        if (s[s.size() - 1] == ')')
-            break;
+    if (i == 1) {
+        file << "X: " << circule.x << endl
+             << "Y: " << circule.y << endl
+             << "Radius: " << circule.radius << endl
+             << "Perimetr: " << circule.perimeter << endl
+             << "Area: " << circule.area << endl;
     }
 }
 
-void OutputList()
+void text(string s)
 {
-    freopen("output.txt", "w", stdout);
-    cout << s;
-    fclose(stdout);
+    int k = 0;
+    while (s[k] != '(') {
+        k++;
+    }
+    string name = s.substr(0, k);
+    string parametrs;
+    if (name == "circule") {
+        parametrs = s.substr(k, s.size() - k);
+        int first_comma = parametrs.find(',');
+        int second_comma = parametrs.rfind(',');
+        circule.x = stof(parametrs.substr(1, first_comma - 1));
+        circule.y = stof(parametrs.substr(
+                first_comma + 2, second_comma - first_comma - 2));
+        circule.radius = stof(parametrs.substr(
+                second_comma + 2, parametrs.size() - second_comma - 2));
+        circule.perimeter = 2 * M_PI * circule.radius;
+        circule.area = M_PI * circule.radius * circule.radius;
+        print(1);
+    }
 }
-
 int main()
 {
-    InputList();
-    OutputList();
+    string s;
+    while (!F.eof()) {
+        getline(F, s);
+        if (s != "\0") {
+            file << s << endl;
+            text(s);
+        }
+    }
+    F.close();
 }
+
